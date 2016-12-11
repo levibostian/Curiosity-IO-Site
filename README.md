f0d5d6f44f6645ef50f3f89d69a709f99524c59b
Levi Bostian <levi@curiosityio.com>
2016-12-11 16:48:09 -0600

    Remove jekyll for now, move site to where firebase expects it.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Curiosity IO site
+
+
--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var pug = require('gulp-pug');
-var gulp = require('gulp');
-
-gulp.task('pug', function buildHTML() {
-    return gulp.src('/static_site_to_turn_to_jekyll/**/*.pug')
-    .pipe(pug({}))
-});
